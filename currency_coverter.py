ef5319cada987a912159f7f439cc1127e851d53c
AmeenAbdulHakeem <aa5762@g.rit.edu>
2024-03-01 20:20:45 +0400

    activity2

--- currency_coverter.py
+++ currency_coverter.py
@@ -13,29 +13,34 @@ POUNDS_AED = 4.65   # conversion rate - dollar to aed
 """AED to Euro by dividing amount of money entered by the user with Euro Rate"""
 def aed_to_euro(money):
     conv_currency = money / EURO_AED
+
     return conv_currency
     
+"""AED to Pound by dividing amount of money entered by the user with Pound Rate"""
 def aed_to_british_pound(money):
     conv_currency  = money / POUNDS_AED
 
     return conv_currency
     
-    
+"""AED to Dollar by dividing amount of money entered by the user with Dollar Rate"""
 def aed_to_dollar(money):
     conv_currency = money / DOLLARS_AED 
 
     return conv_currency
 
+"""Dollar to AED by multiplying amount of money entered by the user with Dollar Rate"""
 def dollar_to_aed(amount):  
     conv_currency = amount * DOLLARS_AED
 
     return conv_currency    
     
+"""Pound to AED by multiplying amount of money entered by the user with Pound Rate"""
 def british_pound_to_aed(amount):
     conv_currency = amount * POUNDS_AED
 
     return conv_currency
-    
+
+"""Euro to AED by multiplying amount of money entered by the user with Euro Rate"""    
 def euro_to_aed(amount):
     conv_currency =  amount * EURO_AED
 
